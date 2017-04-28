7e0f005d91d4c21e7521d3fe6b59bf064f01ef2e
Laurie Trichet <laurie.trichet@gmail.com>
2017-04-28 22:45:48 +1000

    Add projects files

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour {
+
+	public float speed;
+
+	private Rigidbody rigidBody;
+
+	//called when the first frame is generated.
+	void Start () {
+		rigidBody = GetComponent<Rigidbody> ();
+		rigidBody.velocity = transform.forward * speed;				
+	}
+}
+      
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Boundary{
+	public float xMin, xMax, zMin, zMax;
+}
+
+public class PlayerController : MonoBehaviour {
+
+	private Rigidbody rigidBody;
+
+	public float speed;
+
+	public float tilt;
+
+	public Boundary boundary;
+
+	public GameObject shot;
+	public Transform shotSpwan;
+
+	private float nextFire;
+	public float fireRate;
+
+	void Start () {
+		rigidBody = GetComponent<Rigidbody>();
+	}
+
+	//called once before rendering the frame every frame.
+	void Update () {
+
+		if (Input.GetButton("Fire1") && Time.time > nextFire){
+			nextFire = Time.time + fireRate;
+			//GameObject clone = 
+			Instantiate (shot, shotSpwan.position, shotSpwan.rotation);// as GameObject;
+		}
+	}
+	
+	void FixedUpdate () {
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		rigidBody.velocity = movement * speed;
+
+		rigidBody.position = new Vector3(
+			Mathf.Clamp(rigidBody.position.x, boundary.xMin,  boundary.xMax),
+			0.0f,
+			Mathf.Clamp(rigidBody.position.z,  boundary.zMin,  boundary.zMax)
+		);
+
+		rigidBody.rotation = Quaternion.Euler (0.0f, 0.0f, rigidBody.velocity.x * (-tilt));
+	}
+}
