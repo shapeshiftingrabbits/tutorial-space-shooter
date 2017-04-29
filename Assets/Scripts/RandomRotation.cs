959ed93a3ef6821475f922bae3c36419d5809f9f
Laurie Trichet <laurie.trichet@gmail.com>
2017-04-29 20:27:02 +1000

    add enemy for the player.

---
+++ Assets/Scripts/RandomRotation.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomRotation : MonoBehaviour {
+
+	public float tumble;
+
+	private Rigidbody rigidBody;
+
+	void Start () {
+		rigidBody = GetComponent<Rigidbody>();
+		rigidBody.angularVelocity = Random.insideUnitSphere * tumble;
+	}
+}
