ef13ee5a3930f3348bbd92743489571106493c90
Laurie Trichet <laurie.trichet@gmail.com>
2017-04-30 21:38:17 +1000

    add spwaning waves, add a script to destroy explosion clone

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -6,16 +6,28 @@ public class GameController : MonoBehaviour {
 	public GameObject hazard;
 
 	public Vector3 spawnValues;
+	public int hazardCount;
+	public float spawnWait;
+	public float startWait;
+	public float waveWait;
 
 	void Start () {
-		SpawnWaves ();
+		StartCoroutine (SpawnWaves ());
 	}
 
-	void SpawnWaves (){
-		Vector3 spwanPosition = new Vector3 (Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
-		
-		Quaternion spawnRotation = Quaternion.identity;
+	 IEnumerator SpawnWaves (){
+		 yield return new WaitForSeconds (startWait);
+		 while(true){
 
-		Instantiate (hazard, spwanPosition, spawnRotation);
+			 for (int i = 0; i < hazardCount; ++i ){
+				Vector3 spwanPosition = new Vector3 (Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+		
+				Quaternion spawnRotation = Quaternion.identity;
+
+				Instantiate (hazard, spwanPosition, spawnRotation);
+				yield return new WaitForSeconds (spawnWait);
+			}
+			yield return new WaitForSeconds (waveWait);
+		}
 	}
 }
