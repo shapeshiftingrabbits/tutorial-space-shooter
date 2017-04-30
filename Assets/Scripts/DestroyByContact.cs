387a13b539336f829412c45184995855acf25446
Laurie Trichet <laurie.trichet@gmail.com>
2017-04-30 20:54:26 +1000

    add explosions to the player and the asteroid

---
+++ Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,18 @@
+﻿	using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByContact : MonoBehaviour {
+	public GameObject explosion;
+	public GameObject playerExplosion;
+	 void OnTriggerEnter(Collider other){
+		 if (other.tag == "Boundary") { return; }
+	
+		 Instantiate (explosion, transform.position, transform.rotation);// as GameObject;
+		 if (other.tag == "Player"){
+		 	Instantiate (playerExplosion, other.transform.position, other.transform.rotation);// as GameObject;
+		 }
+		 Destroy(other.gameObject);
+		 Destroy(gameObject);
+	 }
+}
